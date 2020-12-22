e4895df329388f05076d0e7f9e027e2e6202dcc7
ownal-daboubi <owndaboubii@gmil.com>
2020-12-22 20:47:52 +0200

    adding index

---
+++ script.js
@@ -0,0 +1,41 @@
+// var name = prompt("please enter your name");
+// alert ("Hello "+ name);
+// var order = prompt("please enter what do you want to order");
+// alert("your order is "+order);
+
+
+
+// var payment = prompt("how do you wish to pay : creadit card or cash");
+// if (payment == "cash" ) {
+//     alert ("cash")
+// } if (payment == "creadit card") {
+//     alert("creadit card");
+// }else{
+//     alert ("payment method is wrong");
+// }
+
+// var n1 = prompt("please enter the first number");
+// var n2 = prompt("please enter the second number");
+// var method = prompt("please enter caalaculation method: +, -, *, /");
+
+// sum = parseInt(n1) + parseInt(n2);
+// sub = n1 - n2;
+// div = n1 / n2;
+// multi = n1 * n2;
+
+// if (method == "+") {
+//     alert("n1 + n2 = " + sum);
+
+// }
+// if (method == "-") {
+//     alert("n1 - n2 = " + sub);
+
+// }
+// if (method == "*") {
+//     alert("n1 * n2 = " + multi);
+
+// }
+// if (method == "/") {
+//     alert("n1 / n2 = " + div);
+
+// }
